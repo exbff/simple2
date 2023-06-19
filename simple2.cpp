efa1b4e489766bebc16004d2c1793b88d2850bd4
exbff <137104126+exbff@users.noreply.github.com>
2023-06-19 21:22:57 +0300

    Create simple2.cpp

---
+++ simple2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Repeater B)\n";
+    string text;
+    while (true) {
+        cin >> text;
+        cout << text;
+        cout << "\n";
+    }
+}
